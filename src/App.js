bf27cc975460b1568e469ef45f107c4a182b90aa
Muhammad Afzal <mf66680@example.com>
2024-04-05 12:15:12 +0500

    atleq website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import Home from './component/home';
+import Service from './component/service';
+import Contact from './component/service';
+import FrontEnd from './component/frontEnd';
+import SystemDevelopmet from './component/system-development'
+import SEO from './component/seo'
+import './App.css'
+import './index.css'
+import Header from './component/header'
+import Footer from './component/footer'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/front-end" element={<FrontEnd />} />
+        <Route path='/system-development' element={<SystemDevelopmet/>}/>
+        <Route path='/seo' element={<SEO/>}/>  
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
